cdb912c492235c380265ad50bf44745d5119b7f2
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-04-19 13:37:46 -0400

    add extract.py

---
+++ extract.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+import numpy as np
+import pickle as pkl
+
+# # MR_S2
+# data_dir = 'D:/python_project/LandauLevel/data/MR_S2/'
+# T = 0.85    # thickness
+# W = 1.76    # width
+# L = 1.33    # length
+# ch = 2      # \pho_{xx} channel
+
+# MR_S4
+data_dir = 'D:/python_project/LandauLevel/data/MR_S4/'
+T = 0.61    # thickness
+W = 1.81    # width
+L = 0.70    # length
+ch = 1      # Rxx_{xx} channel
+
+resu_name = data_dir[:-1] + '_extracted'
+
+resu = {}
+for file_name in os.listdir(data_dir):
+    data = pd.read_csv(os.path.join(data_dir, file_name))
+    data.rename(columns = {'Temperature (K)': 'Temperature', 
+                           'Field (Oe)': 'Field', 
+                           f'Resistance Ch{ch} (Ohms)': 'rho_xx'},
+                inplace = True)
+    
+    data['Temperature'] = np.round(data['Temperature'], 0)  # round temperature to integer
+    data['Field'] = data['Field']/10000                     # convert field unit from Gauss to Tesla
+    data['rho_xx'] = data['rho_xx'] * W * T / (L * 1000)    # calculate sample resistivity
+
+    rho_xx_0 = np.average(data.loc[(np.abs(data['Field']) <= max(0.05, np.min(np.abs(data['Field']))))]['rho_xx'])
+    rho_xx_pos = data.loc[(data['Field'] > 0)].sort_values(by = 'Field', ascending = True)
+    rho_xx_neg = data.loc[(data['Field'] < 0)].sort_values(by = 'Field', ascending = False)
+    
+    H = np.linspace(0, 9, 1000)
+    rho_xx_pos_new = np.interp(H, rho_xx_pos['Field'], rho_xx_pos['rho_xx'])
+    rho_xx_neg_new = np.interp(H, np.abs(rho_xx_neg['Field']), rho_xx_neg['rho_xx'])
+    
+    rho_xx= (rho_xx_pos_new + rho_xx_neg_new)/2
+    MR = 100*(rho_xx/rho_xx_0 - 1)
+    resu[data['Temperature'][0]] = [H, MR]
+
+with open(resu_name, 'wb') as f:
+    pkl.dump(resu, f)
